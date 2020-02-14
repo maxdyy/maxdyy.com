6862ed224c1e5a545e397b898b3188ef5060bbc3
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2020-02-14 08:33:42 +0100

    post style

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -15,6 +15,9 @@ import {useStyletron} from 'baseui';
 // Components
 import Layout from '../../components/Layout';
 
+// Style
+import {wrapper} from '../../styles/styles';
+
 const Post = ({post}: PostProps) => {
   const {createdAt, postImage, postTitle, postText} = post;
 
@@ -26,12 +29,39 @@ const Post = ({post}: PostProps) => {
   // Style
   const [css, theme] = useStyletron();
 
+  const wrapperStyle = css({
+    ...wrapper,
+    padding: '0 16px 0 24px',
+    boxSizing: 'border-box',
+    margin: '24px auto 96px auto',
+  });
+
+  const imageStyle = css({
+    border: `2px solid ${theme.colors.border}`,
+  });
+
+  const titleStyle = css({
+    color: theme.colors.contentPrimary,
+    margin: '48px 0 0 0',
+  });
+
+  const postStyle = css({
+    color: theme.colors.contentPrimary,
+    margin: '32px 0',
+    textAlign: 'justify',
+  });
+
   return (
     <Layout>
-      <div>
-        <GraphImg image={postImage} />
-        <h1>{postTitle}</h1>
-        <ReactMarkdown source={postText} />
+      <div className={wrapperStyle}>
+        <div className={imageStyle}>
+          <GraphImg image={postImage} />
+        </div>
+        <h1 className={titleStyle}>{postTitle}</h1>
+        <ReactMarkdown
+          source={postText}
+          className={`${postStyle} maxdyy-post-markdown`}
+        />
         <div>{}</div>
       </div>
     </Layout>
