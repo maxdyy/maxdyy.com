30c69e300255aea77dccc56dcb62328b8afded9a
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2020-02-18 08:15:56 +0100

    initial setup about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 
+// Hooks
+import {useStyletron} from 'baseui';
+
 // Components
 import Layout from '../components/Layout';
 
-const About = () => (
-  <Layout>
-    <>About</>
-  </Layout>
-);
+// Style
+import {wrapper} from '../styles/styles';
+
+const About = () => {
+  // Style
+  const [css, theme] = useStyletron();
+  const wrapperStyle = css({
+    ...wrapper,
+    display: 'flex',
+  });
+  const aboutTextWrapper = css({
+    width: '40%',
+  });
+  const aboutImageWrapper = css({
+    width: '60%',
+  });
+
+  return (
+    <Layout>
+      <div className={wrapperStyle}>
+        <div className={aboutTextWrapper}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className={aboutImageWrapper}>
+          <img src="/about-avatar.png" alt="Maksym's photo" />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default About;
